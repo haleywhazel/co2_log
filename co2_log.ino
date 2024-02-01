15751b2d412e1836ac5f74cd648bf25b06be9a75
haleywhazel <hsuweihuang@outlook.com>
2024-02-01 22:54:09 +0000

    Repository setup

---
+++ co2_log.ino
@@ -0,0 +1,39 @@
+#include <MHZ19.h>
+
+#include <Arduino.h>
+#include <MHZ19.h>
+
+#define RX_PIN 10
+#define TX_PIN 11
+#define BAUDRATE 9600   
+
+MHZ19 mhz19;
+#include <SoftwareSerial.h>
+SoftwareSerial mhz19_serial(RX_PIN, TX_PIN);
+
+unsigned long timer = 0;
+bool calibrate = false;
+
+void setup() {
+  Serial.begin(9600);
+  mhz19_serial.begin(BAUDRATE);
+  mhz19.begin(mhz19_serial);
+
+  mhz19.autoCalibration(false);
+
+  if (calibrate) {
+    Serial.println("Calibrating..");
+    mhz19.calibrate();
+  }
+}
+
+void loop() {
+  if (millis() - timer >= 5000) {
+    int co2 = mhz19.getCO2(true);
+    Serial.print(millis());
+    Serial.print(",");
+    Serial.println(mhz19.getCO2());
+
+    timer = millis();
+  }
+}
---
+++ log.py
@@ -0,0 +1,22 @@
+import csv
+import serial
+
+def read_serial(port, baudrate):
+    filename = input("Save as <insert filename>.csv: ") + ".csv"
+    print("Saving data to " + filename + " ...")
+
+    f = open(filename, "x")
+    f.close()
+    ser = serial.Serial(port, baudrate, timeout=0.5)
+
+    while True:
+        data = ser.readline().decode().strip()
+        if data:
+            f = open(filename, "a")
+            f.write(data + "\n")
+            f.close()
+            print(data)
+
+
+if __name__ == '__main__':
+    read_serial('COM4', 9600)
